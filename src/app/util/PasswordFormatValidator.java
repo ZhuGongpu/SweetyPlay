f9ff6ee01c39fc198e0137560fdaa9d6fed22507
lewisKit <lewisfyh2012@gmail.com>
2014-07-13 00:09:14 +0800

    实现密码检测类，保证密码的长度在6~20. 并且保证密码不能全为数字或者字母

--- src/app/util/PasswordFormatValidator.java
+++ src/app/util/PasswordFormatValidator.java
@@ -11,7 +11,28 @@ public class PasswordFormatValidator {
         if (password == null)
             return false;
 
-        //TODO
+        //the length of password should between 6~20
+        int length = password.length();
+        if (length < 6 || length > 20)
+            return false;
+
+        //the password should not be total number or alphas
+        int countNumber = 0, countAlpas = 0;
+
+        byte[] bytes = password.getBytes();
+
+        for (int i = 0; i < password.length(); i++) {
+            if (bytes[i] >= 48 && bytes[i] <= 57)
+                countNumber++;
+
+            if ((bytes[i] >= 65 && bytes[i] <= 90) || (bytes[i] >= 97 && bytes[i] <= 122))
+                countAlpas++;
+        }
+
+        if ((countNumber == password.length()) || (countAlpas == password.length()))
+            return false;
+
+
         return true;
     }
 }
