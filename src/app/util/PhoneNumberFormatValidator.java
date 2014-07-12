fb27dd0606048c79b89c9ead500030058dbe584f
Sky_Yi <skywalker_lty@163.com>
2014-07-12 23:51:08 +0800

    实现手机号验证

--- src/app/util/PhoneNumberFormatValidator.java
+++ src/app/util/PhoneNumberFormatValidator.java
@@ -13,6 +13,30 @@ public class PhoneNumberFormatValidator {
             return false;
 
         //TODO
-        return true;
+        phoneNumber = phoneNumber.trim();
+        if (phoneNumber.startsWith("+")) {//以+号起始
+            phoneNumber = phoneNumber.substring(3, phoneNumber.length() - 1).trim();
+            if (phoneNumber.length() == 11) {
+                char[] phoneNumberArray = phoneNumber.toCharArray();
+                for (int i = 0; i < phoneNumberArray.length; i++) {
+                    if (!(phoneNumberArray[i] >= '0' && phoneNumberArray[i] <= '9')) {
+                        return false;
+                    }
+                }
+                return true;
+            } else
+                return false;
+        } else {
+            if (phoneNumber.length() == 11) {
+                char[] phoneNumberArray = phoneNumber.toCharArray();
+                for (int i = 0; i < phoneNumberArray.length; i++) {
+                    if (!(phoneNumberArray[i] >= '0' && phoneNumberArray[i] <= '9')) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
     }
 }
