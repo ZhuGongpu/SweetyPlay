f9c7f35b9153c68a3db5bc3aae26bd5c1ba0eaef
lewisKit <lewisfyh2012@gmail.com>
2014-07-31 23:06:31 +0800

    将注册界面进行了大幅更改，但风格并没有统一

--- src/app/view/signup/SignUpActivity.java
+++ src/app/view/signup/SignUpActivity.java
@@ -1,20 +1,25 @@
 package app.view.signup;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.*;
 import app.view.login.R;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import app.view.main.MainActivity;
 import avos.AVOSWrapper;
 import avos.callbackwrappers.LogInCallbackWrapper;
 import avos.callbackwrappers.SignUpCallbackWrapper;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVUser;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * sign up界面
@@ -27,17 +32,21 @@ public class SignUpActivity extends Activity {
     /**
      * UI Elements
      */
-    private EditText firstName_editText = null;
-    private EditText lastName_editText = null;
+    private EditText nickname_editText = null;
     private EditText email_editText = null;
-    private RadioButton male_radioButton = null;
-    private RadioButton female_radioButton = null;
     private DatePicker datePicker = null;
+    private EditText birthday_editText = null;
     private EditText country_editText = null;
     private Button play_button = null;
+    private ImageView maleIV, femaleIV;
+    //TODO ADD email
 
-    private String firstName, lastName, countryOrArea, email, phone_number, password, gender = "";
-    private int year, month, day;
+    private String nickName, countryOrArea, email, phone_number, password;
+    private int yearOfBirth, monthOfBirth, dayOfBirth;
+    private String BirthDate;
+    private enum Gender {female, male, unknown}
+    private Gender gender;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,55 +59,41 @@ public class SignUpActivity extends Activity {
         play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firstName = firstName_editText.getText().toString();
-                lastName = lastName_editText.getText().toString();
+                nickName = nickname_editText.getText().toString();
                 email = email_editText.getText().toString();
 
-                if (firstName.length() < 1 || lastName.length() < 1)
-                    lastName_editText.setError("Cannot be NULL");
+                if (!(nickName.length() >= 1 && nickName.length() <= 20))
+                    nickname_editText.setError("Illegal");
                 else if (!datePicker.isSelected())
                     Toast.makeText(getApplicationContext(), "you should pick your birthday first", Toast.LENGTH_SHORT).show();
-                else if (!male_radioButton.isSelected() && !female_radioButton.isSelected())
+                else if (gender == Gender.unknown)
                     Toast.makeText(getApplicationContext(), "you should choose your gender first", Toast.LENGTH_SHORT).show();
-                else if (!isEmail(email)) {
-                    email_editText.setError("Not Valid");
-                } else//注册信息完整
+                else if(!isEmail(email))
                 {
-                    firstName = firstName_editText.getText().toString();
-                    lastName = lastName_editText.getText().toString();
+                    email_editText.setError("Not Valid");
+                }
+                else//注册信息完整
+                {
+                    nickName = nickname_editText.getText().toString();
 
-                    year = datePicker.getYear();
-                    month = datePicker.getMonth();
-                    day = datePicker.getDayOfMonth();
+
 
                     countryOrArea = country_editText.getText().toString();
                     email = email_editText.getText().toString();
 
-                    if (male_radioButton.isChecked())
-                        gender = "男";
-                    else if (female_radioButton.isChecked())
-                        gender = "女";
-
                     //初始化avos
                     AVOSWrapper.init(SignUpActivity.this);
 
-                    //set user info
+                    //todo set user info
                     AVUser user = new AVUser();
-                    user.setEmail(email);
-                    user.setPassword(password);
-                    user.setUsername(lastName + firstName);//todo 待定
 
-                    user.put("birthday", year + "-" + month + "-" + day);
-                    user.put("gender", gender);
-                    user.put("phone_number", phone_number);
-                    user.put("country", countryOrArea);
 
-                    //sign up
+                    //todo sign up
                     AVOSWrapper.signUpInBackground(user, new SignUpCallbackWrapper() {
                         @Override
                         public void onSucceed() {
                             super.onSucceed();
-                            //succeed
+                            //todo succeed
                             AVOSWrapper.logInInBackground(phone_number, password, new LogInCallbackWrapper() {
 
                                 @Override
@@ -134,26 +129,91 @@ public class SignUpActivity extends Activity {
     }
 
 
-    private void initViews() {
+    private void initViews() {//初始化控件
 
-        firstName_editText = (EditText) findViewById(R.id.firstname_edittext);
-        lastName_editText = (EditText) findViewById(R.id.lastname_edittext);
-        email_editText = (EditText) findViewById(R.id.email_edittext);
+        nickname_editText = (EditText) findViewById(R.id.nickNameET);
+        email_editText = (EditText) findViewById(R.id.emailET);
+        birthday_editText = (EditText)findViewById(R.id.birthdayET);
 
-        male_radioButton = (RadioButton) findViewById(R.id.male_radiobutton);
-        male_radioButton.setSelected(true);
-        female_radioButton = (RadioButton) findViewById(R.id.female_radiobutton);
-
-        datePicker = (DatePicker) findViewById(R.id.datepicker);
-        country_editText = (EditText) findViewById(R.id.country_edittext);
         play_button = (Button) findViewById(R.id.play_button);
+
+        maleIV = (ImageView) findViewById(R.id.gender_male);
+        femaleIV = (ImageView) findViewById(R.id.gender_female);
+
+        gender = Gender.unknown;
+
+        maleIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                maleIV.setImageDrawable(getResources().getDrawable(R.drawable.sex_male_ico1));
+                femaleIV.setImageDrawable(getResources().getDrawable(R.drawable.sex_female_ico0));
+                gender = Gender.male;
+            }
+        });
+
+        femaleIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                femaleIV.setImageDrawable(getResources().getDrawable(R.drawable.sex_female_ico1));
+                maleIV.setImageDrawable((getResources().getDrawable(R.drawable.sex_male_ico0)));
+                gender = Gender.female;
+            }
+        });
+
+        birthday_editText.setInputType(InputType.TYPE_NULL);
+        setCurrentDate();
+        birthday_editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(SignUpActivity.this,
+                        mDateListener , yearOfBirth,
+                        monthOfBirth, dayOfBirth).show();
+
+            }
+        });
 
         Intent intent = this.getIntent();
         phone_number = intent.getStringExtra("phoneNumber");
         password = intent.getStringExtra("password");
     }
 
-    private boolean isEmail(String strEmail) {
+
+    private OnDateSetListener mDateListener = new OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth)
+        {
+            // TODO Auto-generated method stub
+            yearOfBirth = year;
+            monthOfBirth = monthOfYear;
+            dayOfBirth = dayOfMonth;
+            //设置文本显示
+            updateDisplay();
+
+        }
+    };
+
+    //用来设置显示日期格式
+    private void updateDisplay()
+    {
+        this.birthday_editText.setText(new StringBuilder().append(yearOfBirth).append("-")
+                .append((monthOfBirth + 1) < 10 ? "0" + (monthOfBirth + 1) :
+                        (monthOfBirth + 1)).append("-")
+                .append((dayOfBirth < 10) ? "0" + dayOfBirth : dayOfBirth));
+    }
+
+    //获得当前时间
+    private void setCurrentDate()
+    {
+        final Calendar c = Calendar.getInstance();
+        yearOfBirth = c.get(Calendar.YEAR);
+        monthOfBirth = c.get(Calendar.MONTH);
+        dayOfBirth = c.get(Calendar.DAY_OF_MONTH);
+        updateDisplay();
+    }
+
+    private boolean isEmail(String strEmail) {//邮箱格式检测
         String strPattern = "^[a-zA-Z][\\w\\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\\w\\.-]*[a-zA-Z0-9]\\.[a-zA-Z][a-zA-Z\\.]*[a-zA-Z]$";
 
         Pattern p = Pattern.compile(strPattern);
