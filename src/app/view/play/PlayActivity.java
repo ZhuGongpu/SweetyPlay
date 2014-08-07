501686592e2c1524d289666f66c74141b68dc9a9
smallcool <18811399327@163.com>
2014-08-07 10:41:47 +0800

    新增新建活动界面（未完）

---
+++ src/app/view/play/PlayActivity.java
@@ -0,0 +1,41 @@
+package app.view.play;
+
+import android.app.Activity;
+import android.os.Bundle;
+import app.view.login.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * 活动详情页
+ * Created by lxs on 2014/8/4.
+ */
+public class PlayActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity_layout);
+        ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String, Object>>();
+        //测试代码
+        for(int i=0; i<10; i++)
+        {
+            HashMap<String,Object> item=new HashMap<String, Object>();
+            item.put("head",R.drawable.default_photo);
+            list.add(item);
+        }
+        showOnLayout( list);
+
+    }
+
+    private void showOnLayout(ArrayList list)
+    {
+        //判断list长度
+        for(int i=0;i<10;i++)
+        {
+            //从hashmap中取出值
+
+        }
+
+    }
+}
