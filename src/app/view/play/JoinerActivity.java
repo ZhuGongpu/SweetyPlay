5e5cb1b8dc563c58218fe177ae9e74155acaa500
smallcool <18811399327@163.com>
2014-08-08 14:42:23 +0800

    参加者列表 新建活动界面

---
+++ src/app/view/play/JoinerActivity.java
@@ -0,0 +1,65 @@
+package app.view.play;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static app.view.login.R.*;
+
+/**
+ * 参加者列表页
+ * Created by lxs on 2014/8/5.
+ */
+public class JoinerActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.joiner_activity_layout);
+        GridView gridview = (GridView) findViewById(id.joiner_gridview);
+
+        //生成动态数组，并且转入数据
+        ArrayList<HashMap<String, Object>> lstImageItem = new ArrayList<HashMap<String, Object>>();
+        for(int i=0;i<10;i++)
+        {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemImage", drawable.default_photo);//添加图像资源的ID
+            map.put("ItemText", "NO."+String.valueOf(i));//按序号做ItemText
+            lstImageItem.add(map);
+        }
+        //生成适配器的ImageItem <====> 动态数组的元素，两者一一对应
+        SimpleAdapter saImageItems = new SimpleAdapter(this, //没什么解释
+                lstImageItem,//数据来源
+                layout.joiner_gridview_item,//night_item的XML实现
+
+                //动态数组与ImageItem对应的子项
+                new String[] {"ItemImage","ItemText"},
+
+                //ImageItem的XML文件里面的一个ImageView,两个TextView ID
+                new int[] {id.joiner_photo, id.joiner_name});
+        //添加并且显示
+        gridview.setAdapter(saImageItems);
+        //添加消息处理
+        gridview.setOnItemClickListener(new ItemClickListener());
+    }
+
+    //当AdapterView被单击(触摸屏或者键盘)，则返回的Item单击事件
+    class  ItemClickListener implements AdapterView.OnItemClickListener
+    {
+        public void onItemClick(AdapterView<?> arg0,//The AdapterView where the click happened
+                                View arg1,//The view within the AdapterView that was clicked
+                                int arg2,//The position of the view in the adapter
+                                long arg3//The row id of the item that was clicked
+        ) {
+            //在本例中arg2=arg3
+            HashMap<String, Object> item=(HashMap<String, Object>) arg0.getItemAtPosition(arg2);
+            //显示所选Item的ItemText
+            setTitle((String)item.get("ItemText"));
+        }
+
+    }
+}
---
+++ src/app/view/play/NewplayActivity.java
@@ -0,0 +1,111 @@
+package app.view.play;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import app.view.login.R;
+
+
+/**
+ * 新建活动页
+ * Created by lxs on 2014/8/6.
+ */
+
+public class NewplayActivity extends Activity {
+
+    private ImageView photo;
+    private Button enddate, endtime, activitylocation, invitebutton, postbutton;
+    private EditText activityname;
+
+    private static final int DATE_PICKER_ID = 1;
+    private static final int TIME_PICKER_ID = 2;
+
+    private int DATE_YEAR;
+    private int DATE_MONTH;
+    private int DATE_DAY;
+    private int HOUR;
+    private int MINUTE;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newplay_activity_layout);
+
+        photo=(ImageView)findViewById(R.id.newplay_photo);
+        activityname=(EditText)findViewById(R.id.newplay_activityname);
+        enddate=(Button)findViewById(R.id.newplay_enddate);
+        endtime=(Button)findViewById(R.id.newplay_endtime);
+        activitylocation=(Button)findViewById(R.id.newplay_activitylocation);
+        invitebutton=(Button)findViewById(R.id.newplay_invite_button);
+        postbutton=(Button)findViewById(R.id.newplay_post_button);
+
+        dateSetButtonListener datesetlistener = new dateSetButtonListener();
+        timeSetButtonListener timesetlistener = new timeSetButtonListener();
+        enddate.setOnClickListener(datesetlistener);
+        endtime.setOnClickListener(timesetlistener);
+}
+
+//选择活动图片监听器
+class PhotoListener implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+
+    }
+}
+
+    //���ڼ�����ʵ��
+    private class dateSetButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            showDialog(DATE_PICKER_ID);
+        }
+    }
+
+    //�¼�������ʵ��
+    private class timeSetButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            showDialog(TIME_PICKER_ID);
+        }
+    }
+
+    //����ѡ��Ի���ʵ��
+    DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            DATE_YEAR = year;
+            DATE_MONTH = monthOfYear + 1;
+            DATE_DAY = dayOfMonth;
+            enddate.setText("活动截止时间："+DATE_YEAR+"年"+DATE_MONTH+"月"+DATE_DAY+"日");
+        }
+    };
+
+    //ʱ��ѡ��Ի���ѡ��
+    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            HOUR = hourOfDay;
+            MINUTE = minute;
+            endtime.setText(HOUR + ":" + MINUTE);
+        }
+    };
+
+    //�Ի������
+    protected Dialog onCreateDialog(int id){
+        switch(id){
+            case DATE_PICKER_ID:
+                return new DatePickerDialog(this, onDateSetListener , 2014 , 4 , 1 );
+            case TIME_PICKER_ID:
+                return new TimePickerDialog(this, onTimeSetListener , 00 , 00, true);
+        }
+        return null;
+    }
+}
