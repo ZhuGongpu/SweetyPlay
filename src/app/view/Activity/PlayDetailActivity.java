76f64aff0e5837f4b571ea7cb90f0a08b83be759
ZhuGongpu <zhugongpu@hotmail.com>
2014-08-15 00:02:45 +0800

    增加活动详情页

--- src/app/view/Activity/PlayDetailActivity.java
+++ src/app/view/Activity/PlayDetailActivity.java
@@ -2,10 +2,9 @@ package app.view.activity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Point;
+
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +12,7 @@ import android.widget.*;
 import app.view.login.R;
 import avos.AVOSWrapper;
 import avos.callbackwrappers.FindCallbackWrapper;
+import avos.callbackwrappers.SaveCallbackWraprer;
 import avos.models.PlayEntity;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVUser;
@@ -60,28 +60,19 @@ public class PlayDetailActivity extends Activity {
         String playID = getIntent().getStringExtra("PlayID");
 
         initViews();
-        Log.e("PlayDetailActivity", playID);
+        //Log.e("PlayDetailActivity", playID);
         AVOSWrapper.queryPlay(playID, new FindCallbackWrapper<PlayEntity>() {
             @Override
             public void onSucceed(List<PlayEntity> list) {
                 if (list.size() > 0) {
                     currentPlay = list.get(0);
                     setViews();
-                    Log.e(TAG, list.size() + "");
-
                 }
-
-                Log.e(TAG, "on succeed");
             }
 
             @Override
             public void onFailed(AVException e) {
-                /*
-                Log.e("PlayDetailActivity",e.getLocalizedMessage());
-                Toast.makeText(PlayDetailActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                startActivity(new Intent(PlayDetailActivity.this, MainActivity.class));
-                finish();
-                */
+
             }
         });
     }
@@ -92,7 +83,7 @@ public class PlayDetailActivity extends Activity {
     private void initViews() {
         playTitle = (TextView) findViewById(R.id.play_detail_layout_play_title);
         playPhoto = (ImageView) findViewById(R.id.play_detail_layout_play_photo);
-        joinButton = (Button) findViewById(R.id.play_detail_layout_join_button);
+        joinButton = (Button) findViewById(R.id.play_detail_layout_join_button);//TODO 需要根据relation判断是否已经加入
         joinerNumberTextView = (TextView) findViewById(R.id.play_detail_layout_joiner_number_text_view);
         joinerListContainer = (FreeFlowContainer) findViewById(R.id.play_detail_layout_joiner_list_container);
         playDateTextView = (TextView) findViewById(R.id.play_detail_layout_play_date_textview);
@@ -104,42 +95,61 @@ public class PlayDetailActivity extends Activity {
             @Override
             public void onClick(View view) {
                 if (currentPlay != null) {
-                    joinButton.setText("已加入");
                     joinButton.setEnabled(false);
                     //join
-                    currentPlay.getParticipationList().add(AVOSWrapper.getCurrentUser());
-                    currentPlay.setCurrentNumber(currentPlay.getCurrentNumber() + 1);
-                    loadJoinerList();
+                    joinPlay();
+                    currentPlay.saveInBackground(new SaveCallbackWraprer() {
+                        @Override
+                        public void onSucceed() {
+                            joinButton.setText("已加入");
+                            loadJoinerList();
+                            //Log.e(TAG, "已加入");
+                        }
+
+                        @Override
+                        public void onFailed(AVException e) {
+
+                        }
+                    });
                 }
             }
         });
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        HLayout hlayout = new HLayout();
-        hlayout.setLayoutParams(new HLayout.LayoutParams(size.x));
-        joinerListContainer.setLayout(hlayout);
-
-        joinerListAdapter = new JoinerListAdapter(this);
-        joinerListContainer.setAdapter(joinerListAdapter);
     }
 
     /**
      * 设置Views Content
      */
     private void setViews() {
+
+        //Log.e(TAG, "Height : " + joinerListContainer.getHeight());
+        HLayout hlayout = new HLayout();
+        hlayout.setLayoutParams(new HLayout.LayoutParams(joinerListContainer.getHeight()));
+        joinerListContainer.setLayout(hlayout);
+
+        joinerListAdapter = new JoinerListAdapter(this);
+        joinerListContainer.setAdapter(joinerListAdapter);
+
+
         playTitle.setText(currentPlay.getTitle());
         Picasso.with(this).load(currentPlay.getThumbnailUrl()).error(R.drawable.ic_launcher).into(playPhoto);//若加载失败，设置为默认图片
-        joinerNumberTextView.setText(String.format("％s人参加", currentPlay.getCurrentNumber()));
-        playDateTextView.setText(currentPlay.getPlayDate().toString());//TODO format
-        playLocationTextView.setText(currentPlay.getPlace().toString());//TODO convert
+
+        joinerNumberTextView.setText(currentPlay.getCurrentNumber() + "人参加");
+
+        if (currentPlay.getPlayDate() != null)
+            playDateTextView.setText(currentPlay.getPlayDate().toString());//TODO format
+        if (currentPlay.getPlace() != null)
+            playLocationTextView.setText(currentPlay.getPlace().toString());//TODO convert
         playDescriptionTextView.setText(currentPlay.getDescription());
 
         //获取joiner列表
         loadJoinerList();
     }
 
+    /**
+     * 加载JoinerList
+     * 若其中已有数据，则清空原有数据并重新加载
+     */
     private void loadJoinerList() {
         if (currentPlay != null) {
             AVOSWrapper.query(currentPlay.getParticipationList().getQuery(), new FindCallbackWrapper<AVUser>() {
@@ -149,6 +159,8 @@ public class PlayDetailActivity extends Activity {
                         joinerListAdapter.clearData();
                         joinerListAdapter.update(list);
                         joinerListContainer.dataInvalidated();
+                        joinerNumberTextView.setText(currentPlay.getCurrentNumber() + "人参加");
+                        //Log.e(TAG, "loadJoinerList onSucceed");
                     }
                 }
 
@@ -160,6 +172,13 @@ public class PlayDetailActivity extends Activity {
                 }
             });
         }
+    }
+
+    private void joinPlay() {
+        //TODO
+        currentPlay.getParticipationList().add(AVOSWrapper.getCurrentUser());
+        currentPlay.setCurrentNumber(currentPlay.getCurrentNumber() + 1);
+
     }
 
     private class JoinerListAdapter implements SectionedAdapter {
@@ -209,16 +228,23 @@ public class PlayDetailActivity extends Activity {
             ImageView image = (ImageView) convertView.findViewById(R.id.pic);
             TextView title = (TextView) convertView.findViewById(R.id.title);
 
+
             if (hideImages) {
                 int idx = position % colors.length;
                 image.setBackgroundColor(colors[idx]);
 
             } else {
                 AVUser user = (AVUser) this.section.getData().get(position);
-                title.setText(user.getString("nickName"));
-                Picasso.with(context)
-                        .load(AVOSWrapper.getUserAvatarUrl(user, 50, 50))
-                        .into(image);
+
+                if (user != null) {
+                    String url = AVOSWrapper.getUserAvatarUrl(user, 80, 80);
+                    if (url != null) {
+                        title.setText(user.getString("nickName"));
+                        Picasso.with(context)
+                                .load(url)
+                                .into(image);
+                    }
+                }
             }
 
             return convertView;
