6f04a869a195c06813d5fbf6f90b3e16645cccb0
ZhuGongpu <zhugongpu@hotmail.com>
2014-08-15 10:35:22 +0800

    增加活动详情页

--- src/app/view/Activity/PlayDetailActivity.java
+++ src/app/view/Activity/PlayDetailActivity.java
@@ -2,9 +2,7 @@ package app.view.activity;
 
 import android.app.Activity;
 import android.content.Context;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
